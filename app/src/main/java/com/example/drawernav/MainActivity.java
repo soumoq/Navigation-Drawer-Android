e70eba98f9792543e168b45b945416c3cde4c3ff
root <soumojyotiganguly270@gmail.com>
2020-08-03 13:12:48 +0530

    complete drawer nav view

--- app/src/main/java/com/example/drawernav/MainActivity.java
+++ app/src/main/java/com/example/drawernav/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.drawernav;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        final Context context = this;
+        NavigationView navigation =  findViewById(R.id.nav_view);
+        navigation.getMenu().getItem(0).setActionView(R.layout.menu_image);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_message:
+                        startActivity(new Intent(MainActivity.this,MessageActivity.class));
+                        break;
+
+                    case R.id.nav_share:
+                        startActivity(new Intent(MainActivity.this,MessageActivity.class));
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
+
+
 }
